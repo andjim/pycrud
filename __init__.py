79c7764faa9c0d87f923d2f4824a0333dcc8187d
Anderson Jimenez <ajimenez@atrivia.do>
2019-07-23 13:58:58 -0400

    [ADD] - Initial commit

---
+++ __init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy 
+from flask_marshmallow import Marshmallow 
+import os
+
+# Init app
+app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+# Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# Init db
+db = SQLAlchemy(app)
+# Init ma
+ma = Marshmallow(app)
---
+++ controllers/countryController.py
@@ -0,0 +1,26 @@
+from flask import redirect, request, jsonify
+from pycrud import app , db
+from pycrud.models.country import Country , CountrySchema
+
+#Json Schemas
+country_schema = CountrySchema(strict=True)
+countries_schema = CountrySchema(many=True,strict=True)
+
+@app.route('/country', methods=['POST'])
+def create_country():
+    name = request.json['name']
+    status = request.json['status']
+
+    new_country = Country(name, status)
+
+
+    db.session.add(new_country)
+    db.session.commit()
+
+    return country_schema.jsonify(new_country)
+
+@app.route('/country', methods=['GET'])
+def get_products():
+  all_products = Country.query.all()
+  result = countries_schema.dump(all_products)
+  return jsonify(result.data)
